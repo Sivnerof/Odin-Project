7f7fd4cadf58b5a05625878487023ce647698902
Rogue Blue <rogueblueprint@protonmail.com>
2022-01-19 13:45:44 -0700

    Regex tutorial finished

--- Regex-Form/validation.js
+++ Regex-Form/validation.js
@@ -1,1 +1,26 @@
-// validation script here
+// Validation Script Here
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+};
+
+// Validation Function
+function validate(field, regex){
+    if (regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        //console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
