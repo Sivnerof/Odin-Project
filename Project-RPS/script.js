e64ae130f91b19620f73178ecd5e271473ee4ab1
Rogue Blue <rogueblueprint@protonmail.com>
2022-01-25 14:40:15 -0700

    Roshambo part one completed

--- Project-RPS/script.js
+++ Project-RPS/script.js
@@ -1,40 +1,41 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay(){
     const availableChoices = ["rock", "paper", "scissors"];
     const computerChoice = availableChoices[Math.floor(Math.random() * availableChoices.length)];
     return computerChoice;
 }
 
+
 function playRound(computerSelection, playerSelection){
     const playerChoice = playerSelection.toLowerCase();
     console.log(computerSelection, playerSelection)
     if (playerChoice === computerSelection){
-        return `${playerChoice} vs ${computerSelection}: Tie Game!`;
+        console.log(`${playerChoice} vs ${computerSelection}: Tie Game!`);
     } else if (playerChoice === "rock" && computerSelection === "scissors" ||
             playerChoice === "paper" && computerSelection === "rock" || 
             playerChoice === "scissors" && computerSelection === "paper"){
-        return `${playerChoice} beats ${computerSelection}, Player Wins!!!`;
+        console.log(`${playerChoice} beats ${computerSelection}, Player Wins!!!`);
+        playerScore++;
     } else {
-        return `${computerSelection} beats ${playerChoice}, Computer Wins!!!`;
+        console.log(`${computerSelection} beats ${playerChoice}, Computer Wins!!!`);
+        computerScore++;
     }
 }
 
-console.log(playRound(computerPlay(), prompt("Rock, Paper, or Scissors?", "")));
-/*    
-    Important note: you want to return the results of this function call, not console.log() them. 
-    To test this function console.log the results:
-    
-    Write a NEW function called game(). 
-    Use the previous function inside of this one to play a 5 round game that 
-    keeps score and reports a winner or loser at the end.
-    
-    You have not officially learned how to “loop” over code to repeat function calls… 
-    if you already know about loops from somewhere else 
-    (or if you feel like doing some more learning) feel free to use them. 
-    If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-    
-    At this point you should be using console.log() to 
-    display the results of each round and the winner at the end.
-    
-    Feel free to re-work your previous functions if you need to. 
-    Specifically, you might want to change the return value to something more useful.
-*/
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        playRound(computerPlay(), prompt("Rock, Paper, or Scissors?", ""));
+    }
+    if (playerScore > computerScore){
+        console.log(`FINAL SCORE: Player Wins! ${playerScore} vs ${computerScore}`);
+    } else if (computerScoreScore > playerScore) {
+        console.log(`FINAL SCORE: Computer Wins! ${computerScore} vs ${playerScore}`);
+    } else {
+        console.log(`Tie Game`);
+    }
+}
+
+game();
