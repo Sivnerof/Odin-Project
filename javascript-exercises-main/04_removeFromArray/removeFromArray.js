2a4c3ff20274593601fdfd8d24e91573f6ad8265
Rogue Blue <rogueblueprint@protonmail.com>
2022-01-27 16:25:19 -0700

    Remove From Array Function

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    const elementsToSearch = [...elements];
+    for (let i = 0; i < elementsToSearch.length; i++){
+        for (let j = 0; j < array.length; j++){
+            if(elementsToSearch[i] === array[j]){
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
 };
+
+/*
+for (const cat of cats) {
+  console.log(cat);
+}
+
+- how to remove a single element from an array
+- how to deal with multiple optional arguments in a javascript function
+- (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments).  
+- Scroll down to the bit about `Array.from` or the spread operator. 
+- [Or this link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters).
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
