3ee3e5425163307e88f4fd8bd1c0857beb82fe4c
Rogue Blue <rogueblueprint@protonmail.com>
2022-01-28 10:13:27 -0700

    Leap Years

--- javascript-exercises-main/06_leapYears/leapYears.js
+++ javascript-exercises-main/06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap;
+    if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)){
+        return leap = true;
+    } else {
+        return leap = false;
+    }
 };
 
 // Do not edit below this line
