e522be0ac2e845b2674d75a1ec3d1618f976b77f
Rogue Blue <rogueblueprint@protonmail.com>
2022-02-26 17:58:19 -0700

    Changed regex patter for phone number

--- Regex-Form/validation.js
+++ Regex-Form/validation.js
@@ -2,7 +2,7 @@
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
-    telephone: /^\d{11}$/,
+    telephone: /^\d{10}$/,
     username: /^[a-z\d]{5,12}$/i,
     password: /^[\w@-]{8,20}$/,
     slug: /^[a-z\d-]{8,20}$/,
