a01a9b1bee5409a1ff6943fd587e43cc0352cd6a
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-07 21:44:10 -0700

    Added calculator exercise solutions

--- javascript-exercises-main/08_calculator/calculator.js
+++ javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	array_sum = 0;
+  for(let i = 0; i < array.length; i++){
+    array_sum += array[i];
+  }
+  return array_sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  array_product = 1;
+  for(let i = 0; i < array.length; i++){
+    array_product *= array[i];
+  }
+  return array_product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0){
+    return 'Only Positive Integers';
+  } else if (num == 0){
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
