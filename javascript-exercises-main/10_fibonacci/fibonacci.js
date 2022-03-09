cef7912fd0842a8457e5a02bc481bb1ab3eba9d7
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-08 21:12:12 -0700

    Fibonacci Solution Added

--- javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = +num;
+    if (fib < 0){
+        return 'OOPS'
+    } else if (fib <= 1){
+        return fib;
+    } else {
+        return fibonacci(fib - 1) + fibonacci(fib - 2);
+    }
 };
 
 // Do not edit below this line
