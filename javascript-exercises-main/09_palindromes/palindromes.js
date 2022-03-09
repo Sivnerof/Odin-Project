66040268f20e1d5179e0525f95ac2817a690e85a
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-08 20:32:42 -0700

    Palindrome Exercise Solution Uploaded

--- javascript-exercises-main/09_palindromes/palindromes.js
+++ javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const regex = /[^A-Za-z]/g;
+    const new_str = string.split(' ').join('').replace(regex, "").toUpperCase();
+    const reversed_string = new_str.split('').reverse().join("");
+    
+    if (new_str !== reversed_string){
+        return false;
+    }
 
+    return true;
 };
 
 // Do not edit below this line
