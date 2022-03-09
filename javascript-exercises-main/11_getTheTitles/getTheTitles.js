ff4d05c23301c215f919facc3fe59fc20ab15008
Rogue Blue <rogueblueprint@protonmail.com>
2022-03-08 21:48:15 -0700

    Book List Soultion Added

--- javascript-exercises-main/11_getTheTitles/getTheTitles.js
+++ javascript-exercises-main/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let book_list = [];
+    for(let i = 0; i < books.length; i++){
+        book_list.push(books[i]['title']);
+    }
+    return book_list;
 };
 
 // Do not edit below this line
